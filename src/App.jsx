62bb1ceb26ec37e731525eebb276575ea3477f04
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-11-11 18:30:21 +0700

    Add randomize index of answers array & improve UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import axios from 'axios';
 
 function App() {
 
+  // Initiate questions object that storing data for questions.
   const [questions, setQuestions] = useState({
     question: '',
     correctAnswer: '',
-    incorrectAnswers: '',
-    answers: ''
-  })
+    incorrectAnswers: [],
+    answers: []
+  });
 
-  // const [noQuestion, setNoQuestion] = useState(0)
+  // Initiate isLoading for doing loading state.
+  const [isLoading, setIsLoading] = useState(true);
 
-  const [isLoading, setIsLoading] = useState(true)
+  // Initiate userAnswer for input answer by user.
+  const [userAnswer, setUserAnswer] = useState('');
 
-  const [userAnswer, setUserAnswer] = useState('')
-
+  // Method to get data question form the trivia api and changing isLoading state.
   const getQuestion = () => {
+    setIsLoading(true);
     axios
     .get('https://the-trivia-api.com/v2/questions/')
     .then(response => {
@@ -24,7 +27,7 @@ function App() {
         question: response.data[0].question.text,
         correctAnswer: response.data[0].correctAnswer,
         incorrectAnswers: response.data[0].incorrectAnswers,
-        answers: [...response.data[0].incorrectAnswers, response.data[0].correctAnswer]
+        answers: [...response.data[0].incorrectAnswers, response.data[0].correctAnswer].sort(() => Math.random() - 0.5)
       })
       setIsLoading(false)
     })
@@ -34,26 +37,31 @@ function App() {
     });
   }
 
+  // Do getQuestion method when web rendered using useEffect.
   useEffect(() => {
     getQuestion()
-  }, [])
+  }, []);
 
+  // Handler for changing user answer value.
   const handleUserAnswer = (event) => {
     setUserAnswer(event.target.value)
-  }
+  };
 
+  // Methor for submit the answer. And check if user answer is same with the correct answer.
   const submitAnswer = () => {
-    if(questions.correctAnswer === userAnswer){
+    if(!userAnswer){
+      alert('Please Select Your Answer')
+    } else if (userAnswer === questions.correctAnswer) {
       alert('Yeayy!!! Your Answer Correct!!')
     } else {
       alert('Sorry Your Answer is Incorrect:(')
     }
-  }
+  };
 
   return (
     <>
-      <div className='bg-gray-100 h-full flex justify-center items-center text-gray-800'>
-        <div className='bg-gray-200 border-2 border-stone-950 rounded-lg h-5/6 w-5/6 flex flex-col p-8'>
+      <div className='bg-gray-700 h-full flex justify-center items-center text-gray-800'>
+        <div className='bg-gray-100 border-4 border-stone-950 rounded-lg h-5/6 w-5/6 flex flex-col p-8'>
           <div className='basis-2/5 p-4 flex justify-center items-center'>
             {isLoading ? (
               <p className='text-2xl font-bold'>
@@ -88,15 +96,15 @@ function App() {
             <div className='flex flex-col gap-8 basis-2/5 px-8 font-semibold'>
               {questions.answers.map((ele, index) => (
                 <label htmlFor={index} key={index} className='flex gap-4 text-xl'>
-                  <input type="checkbox" name="answer" id={index} className='w-8' value={ele} onChange={handleUserAnswer} checked={userAnswer === ele}/>
-                  <span>{ele}</span>
+                  <input type="checkbox" name="answer" id={index} className='w-8 focus:outline-none ' value={ele} onChange={handleUserAnswer} checked={userAnswer === ele} onKeyPress={(e) => { if(e.key === 'Enter') {submitAnswer()} }}/>
+                  <span className='hover:text-blue-900'>{ele}</span>
                 </label>
               ))}
             </div>
           )}
-          <div className='flex justify-end basis-1/5 text-xl font-bold'>
-            <button onClick={submitAnswer} className='border-2 bg-cyan-600 rounded-md px-4 py-2 h-fit justify-self-end'>Answer</button>
-            <button className='border-2 bg-red-600 rounded-md px-4 py-2 h-fit justify-self-end' onClick={getQuestion}>Change Question</button>
+          <div className='flex justify-end items-end basis-1/5 gap-5 text-xl font-bold'>
+            <button onClick={submitAnswer} className='border-2 bg-cyan-600 rounded-md px-4 py-2 h-fit justify-self-end hover:bg-cyan-900 hover:text-black'>Answer</button>
+            <button onClick={getQuestion} className='border-2 bg-red-600 rounded-md px-4 py-2 h-fit justify-self-end hover:bg-red-900 hover:text-black'>Change Question</button>
           </div>
         </div>
       </div>

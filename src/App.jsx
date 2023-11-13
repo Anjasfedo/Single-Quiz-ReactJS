172dbe0025340ab2f7289d8879989e1710fc8c35
G1A021037-ANJASFEDO <fedoaf98@gmail.com>
2023-11-13 14:37:31 +0700

    Try bug & update button

--- src/App.jsx
+++ src/App.jsx
@@ -19,22 +19,23 @@ function App() {
 
   // Method to get data question form the trivia api and changing isLoading state.
   const getQuestion = () => {
+    const randomIndex = Math.floor(Math.random() * 5)
     setIsLoading(true);
     axios
-    .get('https://the-trivia-api.com/v2/questions/')
-    .then(response => {
-      setQuestions({
-        question: response.data[0].question.text,
-        correctAnswer: response.data[0].correctAnswer,
-        incorrectAnswers: response.data[0].incorrectAnswers,
-        answers: [...response.data[0].incorrectAnswers, response.data[0].correctAnswer].sort(() => Math.random() - 0.5)
+      .get('https://the-trivia-api.com/v2/questions/')
+      .then(response => {
+        setQuestions({
+          question: response.data[randomIndex].question.text,
+          correctAnswer: response.data[randomIndex].correctAnswer,
+          incorrectAnswers: response.data[randomIndex].incorrectAnswers,
+          answers: [...response.data[randomIndex].incorrectAnswers, response.data[0].correctAnswer].sort(() => Math.random() - 0.5)
+        })
+        setIsLoading(false)
       })
-      setIsLoading(false)
-    })
-    .catch((error) => {
-      console.log(error)
-      setIsLoading(false)
-    });
+      .catch((error) => {
+        console.log(error)
+        setIsLoading(false)
+      });
   }
 
   // Do getQuestion method when web rendered using useEffect.
@@ -49,12 +50,13 @@ function App() {
 
   // Methor for submit the answer. And check if user answer is same with the correct answer.
   const submitAnswer = () => {
-    if(!userAnswer){
-      alert('Please Select Your Answer')
+    if (!userAnswer) {
+      alert('Please Select Your Answer');
     } else if (userAnswer === questions.correctAnswer) {
-      alert('Yeayy!!! Your Answer Correct!!')
+      alert('Yeayy!!! Your Answer Correct!!');
+      getQuestion();
     } else {
-      alert('Sorry Your Answer is Incorrect:(')
+      alert('Sorry Your Answer is Incorrect:(');
     }
   };
 
@@ -62,49 +64,52 @@ function App() {
     <>
       <div className='bg-gray-700 h-full flex justify-center items-center text-gray-800'>
         <div className='bg-gray-100 border-4 border-stone-950 rounded-lg h-5/6 w-5/6 flex flex-col p-8'>
-          <div className='basis-2/5 p-4 flex justify-center items-center'>
-            {isLoading ? (
-              <p className='text-2xl font-bold'>
-              Loading..
-            </p>
-            ) : (
-              <p className='text-2xl font-bold'>
-              {questions.question}
-            </p>
-            )}
-          </div>
           {isLoading ? (
-            <div className='flex flex-col gap-8 basis-2/5 px-8 font-semibold'>
-              <label htmlFor="answer" className='flex gap-4 text-xl'>
-                <input type="checkbox" name="answer" id="" className='w-8' />
-                <span>Loading..</span>
-              </label>
-              <label htmlFor="" className='flex gap-4 text-xl'>
-                <input type="checkbox" name="answer" id="" className='w-8' />
-                <span>Loading..</span>
-              </label>
-              <label htmlFor="" className='flex gap-4 text-xl'>
-                <input type="checkbox" name="answer" id="" className='w-8' />
-                <span>Loading..</span>
-              </label>
-              <label htmlFor="" className='flex gap-4 text-xl'>
-                <input type="checkbox" name="answer" id="" className='w-8' />
-                <span>Loading..</span>
-              </label>
-            </div>
-          ) : (
-            <div className='flex flex-col gap-8 basis-2/5 px-8 font-semibold'>
-              {questions.answers.map((ele, index) => (
-                <label htmlFor={index} key={index} className='flex gap-4 text-xl'>
-                  <input type="checkbox" name="answer" id={index} className='w-8 focus:outline-none ' value={ele} onChange={handleUserAnswer} checked={userAnswer === ele} onKeyPress={(e) => { if(e.key === 'Enter') {submitAnswer()} }}/>
-                  <span className='hover:text-blue-900'>{ele}</span>
+            <>
+              <div className='basis-2/5 p-4 flex justify-center items-center'>
+                <p className='text-2xl font-bold'>
+                  Loading..
+                </p>
+              </div>
+              <div className='flex flex-col gap-8 basis-2/5 px-8 font-semibold'>
+                <label htmlFor="answer" className='flex gap-4 text-xl'>
+                  <input type="checkbox" name="answer" id="" className='w-8' />
+                  <span>Loading..</span>
                 </label>
-              ))}
-            </div>
+                <label htmlFor="" className='flex gap-4 text-xl'>
+                  <input type="checkbox" name="answer" id="" className='w-8' />
+                  <span>Loading..</span>
+                </label>
+                <label htmlFor="" className='flex gap-4 text-xl'>
+                  <input type="checkbox" name="answer" id="" className='w-8' />
+                  <span>Loading..</span>
+                </label>
+                <label htmlFor="" className='flex gap-4 text-xl'>
+                  <input type="checkbox" name="answer" id="" className='w-8' />
+                  <span>Loading..</span>
+                </label>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='basis-2/5 p-4 flex justify-center items-center'>
+                <p className='text-2xl font-bold'>
+                  {questions.question}
+                </p>
+              </div>
+              <div className='flex flex-col gap-8 basis-2/5 px-8 font-semibold'>
+                {questions.answers.map((ele, index) => (
+                  <label htmlFor={index} key={index} className='flex gap-4 text-xl'>
+                    <input type="checkbox" name="answer" id={index} className='w-8 focus:outline-none ' value={ele} onChange={handleUserAnswer} onKeyPress={(e) => { if (e.key === 'Enter') { submitAnswer() } }} />
+                    <span className='hover:text-blue-900'>{ele}</span>
+                  </label>
+                ))}
+              </div>
+            </>
           )}
-          <div className='flex justify-end items-end basis-1/5 gap-5 text-xl font-bold'>
-            <button onClick={submitAnswer} className='border-2 bg-cyan-600 rounded-md px-4 py-2 h-fit justify-self-end hover:bg-cyan-900 hover:text-black'>Answer</button>
-            <button onClick={getQuestion} className='border-2 bg-red-600 rounded-md px-4 py-2 h-fit justify-self-end hover:bg-red-900 hover:text-black'>Change Question</button>
+          <div className='flex justify-end items-end basis-1/5 gap-5 text-xl font-bold text-white'>
+            <button onClick={submitAnswer} className='border-2 border-black bg-cyan-600 rounded-md px-4 py-2 h-fit justify-self-end hover:bg-cyan-900 hover:text-black'>Answer</button>
+            <button onClick={getQuestion} className='border-2 border-black bg-red-600 rounded-md px-4 py-2 h-fit justify-self-end hover:bg-red-900 hover:text-black'>Change Question</button>
           </div>
         </div>
       </div>
